ade026a65854ca1c0999bc81d1a9b450f7893c71
James Scott <jsa.codes@gmail.com>
2023-02-03 11:56:55 -0600

    Added comments to JS file

--- Wave-Login/script.js
+++ Wave-Login/script.js
@@ -1,5 +1,7 @@
+// This gives a node list of all of the labels
 const labels = document.querySelectorAll('.form-control label');
 
+// Since it's a node list we can use forEach.
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
