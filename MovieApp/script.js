5fc76899c7748376767967eb0e5e68b5fc3b76bf
James Scott <jsa.codes@gmail.com>
2023-03-12 11:59:00 -0500

    Adding comments to JS file to clarify functionality

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -1,8 +1,27 @@
+
+/* 
+- BREAKDOWN OF THE API URL
+- API is the subdomain of the domain themoviedb.org
+- /3 is the version of the API
+- discover/movie?sort_by=popularity (the query string)
+- Next we have our API key
+- And last is the '&page=1' which gives us the first page of results of the most popular movies
+*/
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7f30b0672ba157c6ae514e8210a9a240&page=1'
 
+/* 
+- Setting the size of the image 
+- We don't need the slash after the width 
+*/
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 
+/* 
+- For search functionality 
+- We also want to leave the single double quote at the end so that we can concatenate a search term from the search bar into the string below.
+*/
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=7f30b0672ba157c6ae514e8210a9a240&query="'
+
+
 
 
 const main = document.getElementById('main')
@@ -10,24 +29,27 @@ const form = document.getElementById('form')
 const search = document.getElementById('search')
 
 
+
+
 // Get initial movies
 getMovies(API_URL)
 
 // GET MOVIES FUNCTION
 async function getMovies(url) {
-    const res = await fetch(url)
-    const data = await res.json()
+    const response = await fetch(url)
+    const data = await response.json()
 
     showMovies(data.results)
 
     console.log(data.results);
 }
 
+// Function to create a list of movies that match the search criteria
 function showMovies(movieList) {
     main.innerHTML = ''
 
     movieList.forEach((movie) => {
-        // using destructuring
+        // using destructuring to get values out of the movie object
         const { title, poster_path, vote_average, overview } = movie
 
         const movieEl = document.createElement('div')
